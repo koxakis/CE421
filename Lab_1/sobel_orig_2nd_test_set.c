a79dcb39a94b23ed9cf711b2b9b88d87268ba2a4
Nikolas Koxenoglou <koxakin@gmail.com>
2016-10-15 00:18:41 +0300

    Updated enviroment for 2nd experiment tests

--- Lab_1/sobel_orig_2nd_test_set.c
+++ Lab_1/sobel_orig_2nd_test_set.c
@@ -13,11 +13,11 @@
 #define GOLDEN_FILE	"golden.grey"
 
 /* The horizontal and vertical operators to be used in the sobel filter */
-char horiz_operator[3][3] = {{-1, 0, 1}, 
-                             {-2, 0, 2}, 
+char horiz_operator[3][3] = {{-1, 0, 1},
+                             {-2, 0, 2},
                              {-1, 0, 1}};
-char vert_operator[3][3] = {{1, 2, 1}, 
-                            {0, 0, 0}, 
+char vert_operator[3][3] = {{1, 2, 1},
+                            {0, 0, 0},
                             {-1, -2, -1}};
 
 double sobel(unsigned char *input, unsigned char *output, unsigned char *golden);
@@ -39,11 +39,15 @@ unsigned char input[SIZE*SIZE], output[SIZE*SIZE], golden[SIZE*SIZE];
  * pixel we process.														  */
 int convolution2D(int posy, int posx, const unsigned char *input, char operator[][3]) {
 	int i, j, res;
-  
+
 	res = 0;
 	for (j = -1; j <= 1; j++) {
 		for (i = -1; i <= 1; i++) {
 			res += input[(posy + i)*SIZE + posx + j] * operator[i+1][j+1];
+
+			res += input[(posy + i)*SIZE + posx + (j+1)] * operator[i+1][j+2];
+
+			res += input[(posy + i)*SIZE + posx + (j+2)] * operator[i+1][j+3];
 		}
 	}
 	return(res);
@@ -80,27 +84,27 @@ double sobel(unsigned char *input, unsigned char *output, unsigned char *golden)
 		printf("File " INPUT_FILE " not found\n");
 		exit(1);
 	}
-  
+
 	f_out = fopen(OUTPUT_FILE, "wb");
 	if (f_out == NULL) {
 		printf("File " OUTPUT_FILE " could not be created\n");
 		fclose(f_in);
 		exit(1);
-	}  
-  
+	}
+
 	f_golden = fopen(GOLDEN_FILE, "r");
 	if (f_golden == NULL) {
 		printf("File " GOLDEN_FILE " not found\n");
 		fclose(f_in);
 		fclose(f_out);
 		exit(1);
-	}    
+	}
 
 	fread(input, sizeof(unsigned char), SIZE*SIZE, f_in);
 	fread(golden, sizeof(unsigned char), SIZE*SIZE, f_golden);
 	fclose(f_in);
 	fclose(f_golden);
-  
+
 	/* This is the main computation. Get the starting time. */
 	clock_gettime(CLOCK_MONOTONIC_RAW, &tv1);
 	/* For each pixel of the output image */
@@ -108,13 +112,13 @@ double sobel(unsigned char *input, unsigned char *output, unsigned char *golden)
 		for (i=1; i<SIZE-1; i+=1 ) {
 			/* Apply the sobel filter and calculate the magnitude *
 			 * of the derivative.								  */
-			p = pow(convolution2D(i, j, input, horiz_operator), 2) + 
+			p = pow(convolution2D(i, j, input, horiz_operator), 2) +
 				pow(convolution2D(i, j, input, vert_operator), 2);
 			res = (int)sqrt(p);
 			/* If the resulting value is greater than 255, clip it *
 			 * to 255.											   */
 			if (res > 255)
-				output[i*SIZE + j] = 255;      
+				output[i*SIZE + j] = 255;
 			else
 				output[i*SIZE + j] = (unsigned char)res;
 		}
@@ -128,7 +132,7 @@ double sobel(unsigned char *input, unsigned char *output, unsigned char *golden)
 			PSNR += t;
 		}
 	}
-  
+
 	PSNR /= (double)(SIZE*SIZE);
 	PSNR = 10*log10(65536/PSNR);
 
@@ -140,11 +144,11 @@ double sobel(unsigned char *input, unsigned char *output, unsigned char *golden)
 			(double) (tv2.tv_nsec - tv1.tv_nsec) / 1000000000.0 +
 			(double) (tv2.tv_sec - tv1.tv_sec));
 
-  
+
 	/* Write the output file */
 	fwrite(output, sizeof(unsigned char), SIZE*SIZE, f_out);
 	fclose(f_out);
-  
+
 	return PSNR;
 }
 
@@ -158,4 +162,3 @@ int main(int argc, char* argv[])
 
 	return 0;
 }
-
