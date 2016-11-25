23c49a839bec1bc87f4832f3f93af3b8d5b7a1f6
Nikolas Koxenoglou <koxakin@gmail.com>
2016-11-25 23:53:57 +0200

    First GPU compile

--- Lab_4/parallel_code/main.cu
+++ Lab_4/parallel_code/main.cu
@@ -3,18 +3,15 @@
 #include <stdlib.h>
 #include "hist-equ.h"
 
-//void run_cpu_gray_test(PGM_IMG img_in, char *out_filename);
-
-void histogram(int * hist_out, unsigned char * img_in, int img_size, int nbr_bin){
-    int i;
-    for ( i = 0; i < nbr_bin; i ++){
-        hist_out[i] = 0;
+#define cudaCheckError() {                                                                       \
+        cudaError_t e=cudaGetLastError();                                                        \
+        if(e!=cudaSuccess) {                                                                     \
+            printf("Cuda failure %s:%d: '%s'\n",__FILE__,__LINE__,cudaGetErrorString(e));        \
+            exit(EXIT_FAILURE);                                                                  \
+        }                                                                                        \
     }
 
-    for ( i = 0; i < img_size; i ++){
-        hist_out[img_in[i]] ++;
-    }
-}
+
 
 __global__ void histogramGPU ( int * d_hist_out, unsigned char * d_img_in, int img_size, int nbr_bin) {
 	/* code */
@@ -25,75 +22,79 @@ __global__ void histogram_equalizationGPU ( unsigned char * d_img_out, unsigned 
 	/* code */
 }
 
-void histogram_equalization(unsigned char * img_out, unsigned char * img_in,
-                            int * hist_in, int img_size, int nbr_bin){
-
-	int *lut = (int *)malloc(sizeof(int)*nbr_bin);
-    int i, cdf, min, d;
-    /* Construct the LUT by calculating the CDF */
-    cdf = 0;
-    min = 0;
-    i = 0;
-    while(min == 0){
-        min = hist_in[i++];
-    }
-    d = img_size - min;
-    for(i = 0; i < nbr_bin; i ++){
-        cdf += hist_in[i];
-        //lut[i] = (cdf - min)*(nbr_bin - 1)/d;
-        lut[i] = (int)(((float)cdf - min)*255/d + 0.5);
-        if(lut[i] < 0){
-            lut[i] = 0;
-        }
-
-
-    }
-
-    /* Get the result image */
-    for(i = 0; i < img_size; i ++){
-        if(lut[img_in[i]] > 255){
-            img_out[i] = 255;
-        }
-        else{
-            img_out[i] = (unsigned char)lut[img_in[i]];
-        }
-
-    }
-}
 int main(int argc, char *argv[]){
 	// Host Variables
-    PGM_IMG img_ibuf_g;
+    PGM_IMG h_img_in;
 	unsigned int timer = 0;
-    PGM_IMG img_obuf;
+    PGM_IMG h_img_out_buf;
 
-	//from contrast enhancement
-	PGM_IMG result;
     int hist[256];
 
-	result.w = img_in.w;
-	result.h = img_in.h;
-	result.img = (unsigned char *)malloc(result.w * result.h * sizeof(unsigned char));
+	// Device Variables
+	int *d_hist_in,
+		*d_img_in;
+
+	unsigned char *d_output;
+
 
 	if (argc != 3) {
 		printf("Run with input file name and output file name as arguments\n");
 		exit(1);
 	}
+    h_img_in = read_pgm(argv[1]);
 
-    printf("Running contrast enhancement for gray-scale images.\n");
-    img_ibuf_g = read_pgm(argv[1]);
+	h_img_out_buf.w = h_img_in.w;
+	h_img_out_buf.h = h_img_in.h;
+
+	printf("Allocating host memory...\n");
+	//Host memory allocation
+	h_img_out_buf.img = (unsigned char *)malloc(result.w * result.h * sizeof(unsigned char));
+	if ( h_img_out_buf == NULL){
+		fprintf(stderr, "Failed to allocate Host memory!\n");
+        exit(EXIT_FAILURE);
+	}
+
+	//Device memory allocation
+	printf("Allocating Device arrays...\n");
+	d_hist_out = NULL;
+	cudaMalloc((void **)&d_hist_out, 256 * sizeof(int));
+	cudaCheckError();
+
+	d_img_in = NULL;
+	cudaMalloc((void **)&d_img_in, h_img_in.h * h_img_in.w * sizeof(int));
+	cudaCheckError();
+
+	d_output = NULL;
+	cudaMalloc((void **)&d_output, result.w * result.h * sizeof(unsigned char));
+	cudaCheckError();
+
 	// Main Function call
 	// Data transfer to Device
+	cudaMemcpy(d_img_in, h_img_in, h_img_in.h * h_img_in.w * sizeof(int), cudaMemcpyHostToDevice);
+	cudaCheckError();
 
 	printf("Starting GPU processing...\n");
 	// Redirect to contrast-enhancement.c
-	histogram(hist, img_in.img, img_in.h * img_in.w, 256);
-    histogram_equalization(result.img,img_in.img,hist,result.w*result.h, 256);
+	//Kernel invocations
+	histogramGPU<<<grid, threads>>>(d_hist_out, d_img_in, h_img_in.h * h_img_in.w, 256);
+	cudaCheckError();
 
+	cudaDeviceSynchronize();
+	cudaCheckError();
+
+	histogram_equalizationGPU<<<grid, threads>>>(d_output, d_img_in, d_hist_out, h_img_in.h * h_img_in.w, 256);
+	cudaCheckError();
+
+	cudaDeviceSynchronize();
+	cudaCheckError();
+
+	//Return Stuff to h_img_out_buf
+	cudaMemcpy(h_img_out_buf, d_output, h_img_in.h * h_img_in.w * sizeof(unsigned char), cudaMemcpyDeviceToHost);
+	cudaCheckError();
 	// I/O Stuff
-    write_pgm(img_ibuf_g, argv[2]);
-    //free_pgm(img_ibuf_g);
-
-    free_pgm(img_ibuf_g);
+    write_pgm(h_img_out_buf, argv[2]);
+	free_pgm(h_img_in);
+    free_pgm(h_img_out_buf);
 
 	return 0;
 }
